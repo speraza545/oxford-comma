1b35004bba55dd7f698128c4476c0fd814d246d1
speraza545 <s.peraza545@gmail.com>
2021-02-23 14:22:21 -0500

    commit tree 271da23532ae4a25c44b2c2bb66cd11f33a53b5b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.size == 2
+        array.join(" and ")
+    elsif array.size == 1
+        array[0]
+    else
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
